fc9fa8184c225283885e32cf93505f65afa17e6d
Yevhen Shemet <yevhene@gmail.com>
2017-10-14 03:21:17 +0300

    Course edit settings

--- lib/novel_web/views/course_view.ex
+++ lib/novel_web/views/course_view.ex
@@ -15,4 +15,18 @@ defmodule NovelWeb.CourseView do
   def course_has_content?(course) do
     present?(course.description)
   end
+
+  def nav_item(conn, text, path) do
+    content_tag :li, class: "nav-item" do
+      link text, to: path, class: nav_item_link_class(conn, path)
+    end
+  end
+
+  defp nav_item_link_class(conn, path) do
+    if path = conn.request_path do
+      "nav-link active"
+    else
+      "nav-link"
+    end
+  end
 end
