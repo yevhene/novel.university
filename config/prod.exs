f01186d0879052f6a5048a0e3724f1de361f83b9
Yevhen Shemet <yevhene@gmail.com>
2017-10-17 01:10:21 +0300

    Degraded to http

--- config/prod.exs
+++ config/prod.exs
@@ -15,8 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :novel, NovelWeb.Endpoint,
   load_from_system_env: true,
-  url: [scheme: "https", host: "novel.university", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [host: "novel.university", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
